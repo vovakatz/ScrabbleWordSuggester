2c1e37c21b40afc3e8820cc643342d50b68d9c8f
Vladimir Katz <vovakats@yahoo.com>
2014-03-14 20:10:29 -0400

    Initial Commit

---
+++ ScrabbleWordSuggester/Indexer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrabbleWordSuggester
+{
+    public class Indexer
+    {
+        public void CreateIndex()
+        {
+            string line;
+            StreamReader file = new StreamReader(Environment.CurrentDirectory + "\\Source\\word_list_moby_crossword.flat.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.");
+            // Suspend the screen.
+            System.Console.ReadLine();
+        }
+
+        private void CalculateRank(string word)
+        {
+            foreach (char c in word.ToLower())
+            {
+                switch (c)
+                    case: 'a'
+            }
+        }
+    }
+}
